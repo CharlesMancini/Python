0c9ca0750ce3381780d48d47855f2aedf4b71f0e
CharlesMancini <154022159+CharlesMancini@users.noreply.github.com>
2024-01-14 15:04:42 -0600

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,6 @@
+from colorama import Fore
+print(Fore.YELLOW + "hello world!")
+print(Fore.RED + "hello world!")
+print(Fore.BLUE + "hello world!")
+print(Fore.CYAN + "hello world!")
+print(Fore.GREEN + "hello world!")
